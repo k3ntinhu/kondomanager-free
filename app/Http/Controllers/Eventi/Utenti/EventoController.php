62975abddf192a4c8b098a57833d9172e500de25
k3ntinhu <k3ntinhu@gmail.com>
2026-03-27 16:21:44 +0000

    sync(eventi): default seguro para edição de recorrência (only_this)

--- app/Http/Controllers/Eventi/Utenti/EventoController.php
+++ app/Http/Controllers/Eventi/Utenti/EventoController.php
@@ -235,7 +235,7 @@ class EventoController extends Controller
             'occurrence_date' => 'nullable|date',
         ]);
 
-        $mode = $request->query('mode', 'all');
+        $mode = $request->query('mode', 'only_this');
         $occurrenceDate = $request->query('occurrence_date', null);
 
         $anagrafica = $this->getUserAnagrafica();
